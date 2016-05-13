8e2d008ad70b3d18f68461a90d0bc016eb3c003a
Ivan <ivanlh20@gmail.com>
2016-05-13 16:01:18 +0200

    MULTEM V2.1 13/05/2016

--- Matlab_functions/Propagator.m
+++
@@ -1,5 +0,0 @@
-function [P] = Propagator(g2, gmax, E_0, z)
-emass = 510.99906;
-hc = 12.3984244;
-lambda = hc/sqrt(E_0*(2*emass + E_0));
-P = exp(-1i*pi*lambda*z*g2); P(g2>gmax^2) = 0;
--- Matlab_functions/ScherzerConditions.m
+++
@@ -1,9 +0,0 @@
-function [f, alpha] = ScherzerConditions(E_0, Cs3)
-% E_0 = acceleration voltage [Kv]
-Cs3 = Cs3*1e+07;
-emass = 510.99906;
-hc = 12.3984244;
-lambda = hc/sqrt(E_0*(2*emass + E_0));
-n = 3/4;
-alpha = (4*(2*n-0.5)*lambda/Cs3)^(1/4); alpha = alpha*1e+03;
-f = sqrt((2*n-0.5)*Cs3*lambda);
