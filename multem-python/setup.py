b0732437373ec7cc1829dcce38223fd40d929328
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-08 15:36:07 +0000

    Export system config and input multislice

--- multem-python/setup.py
+++ multem-python/setup.py
@@ -8,32 +8,6 @@ from setuptools import Command
 from setuptools import find_packages
 
 
-class GcovCommand(Command):
-
-    description = "run gcovr to output C/C++ coverage statistics"
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import os.path
-        from gcovr.__main__ import main
-
-        main(
-            [
-                "--root",
-                os.path.dirname(os.path.abspath(__file__)),
-                "--exclude",
-                ".*CMakeCCompilerId.c$",
-            ]
-        )
-
-
 def main():
     """
     Setup the package
@@ -48,11 +22,9 @@ def main():
         setup_requires=["pytest-runner"],
         tests_require=tests_require,
         test_suite="test",
-        cmdclass={"gcov": GcovCommand},
         extras_require={
             "build_sphinx": ["sphinx", "sphinx_rtd_theme"],
             "test": tests_require,
-            "gcov": ["gcovr"],
         },
     )
 
---
+++ multem-python/src/multem/__init__.py
@@ -0,0 +1,11 @@
+#
+# multem.__init__.py
+#
+# Copyright (C) 2019 Diamond Light Source and Rosalind Franklin Institute
+#
+# Author: James Parkhurst
+#
+# This code is distributed under the GPLv3 license, a copy of
+# which is included in the root directory of this package.
+#
+from multem.multem_ext import *  # noqa
