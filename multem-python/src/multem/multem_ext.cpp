6afa0eed647d45eedce6cbe3da677020f3629cac
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-08 18:01:36 +0000

    Added atom data wrapper

--- multem-python/src/multem/multem_ext.cpp
+++ multem-python/src/multem/multem_ext.cpp
@@ -6,8 +6,11 @@
 #include <pybind11/functional.h>
 #include <multem.h>
 #include <multem/system_configuration.h>
+#include <multem/atom_data.h>
+#include <multem/scanning.h>
 #include <multem/detector.h>
 #include <multem/input_multislice.h>
+#include <multem/output_multislice.h>
 #include <multem/multislice.h>
 
 namespace py = pybind11;
@@ -15,8 +18,11 @@ namespace py = pybind11;
 PYBIND11_MODULE(multem_ext, m)
 {
   export_system_configuration(m);
+  export_atom_data(m);
+  export_scanning(m);
   export_detector(m);
   export_input_multislice(m);
+  export_output_multislice(m);
   export_multislice(m);
 }
 
