1b0a53a404d882f7e2d054aba21e0495e1be19fa
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-11 21:07:18 +0000

    Add range_2d test

---
+++ multem-python/test/test_range_2d.py
@@ -0,0 +1,31 @@
+import pytest
+import multem
+import pickle
+
+
+def test_range_2d():
+
+    range_2d = multem.Range_2d()
+    range_2d.ix_0 = 1
+    range_2d.ix_e = 2
+    range_2d.iy_0 = 3
+    range_2d.iy_e = 4
+    range_2d.ixy_0 = 5
+    range_2d.ixy_e = 6
+
+    def check():
+        assert range_2d.ix_0 == 1
+        assert range_2d.ix_e == 2
+        assert range_2d.iy_0 == 3
+        assert range_2d.iy_e == 4
+        assert range_2d.ixy_0 == 5
+        assert range_2d.ixy_e == 6
+
+    check()
+
+    range_2d = pickle.loads(pickle.dumps(range_2d))
+
+    check()
+
+
+
