fb26488f36a437167fa57f6acca9d2fa7d8e08a9
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-13 13:22:36 +0000

    Removing commented out code

--- multem-python/src/multem/input_multislice.h
+++ multem-python/src/multem/input_multislice.h
@@ -12,28 +12,6 @@
 #ifndef MULTEM_PYTHON_INPUT_MULTISLICE_H
 #define MULTEM_PYTHON_INPUT_MULTISLICE_H
 
-#if defined _WIN32 || defined __CYGWIN__
-  #ifdef BUILDING_DLL
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllexport))
-    #else
-      #define DLL_PUBLIC __declspec(dllexport)
-    #endif
-  #else
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllimport))
-    #else
-      #define DLL_PUBLIC __declspec(dllimport)
-    #endif
-  #endif
-#else
-  #if __GNUC__ >= 4
-    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
-  #else
-    #define DLL_PUBLIC
-  #endif
-#endif
-
 #include <pybind11/pybind11.h>
 #include <multem.h>
 #include <multem/serialization.h>
