896ff03369f740869f4bdc76dada2bd756433fc5
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-15 19:18:58 +0000

    Updated license

--- multem-python/src/multem/ext/lens.h
+++ multem-python/src/multem/ext/lens.h
@@ -1,14 +1,23 @@
 /*
- *  lens.h
+ * This file is part of multem-python.
+ * Copyright 2021 Diamond Light Source
+ * Copyright 2021 Rosalind Franklin Institute
  *
- *  Copyright (C) 2019 Diamond Light Source
+ * Author: James Parkhurst
  *
- *  Author: James Parkhurst
+ * multem-python is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This code is distributed under the GPLv3 license, a copy of
- *  which is included in the root directory of this package.
+ * multem-python is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with multem-python. If not, see <http:// www.gnu.org/licenses/>.
  */
-
 #ifndef MULTEM_PYTHON_LENS_H
 #define MULTEM_PYTHON_LENS_H
 
