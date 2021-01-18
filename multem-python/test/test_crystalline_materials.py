5d1ab686c67de6cd61b7d21be7e16d71536e3612
James Parkhurst <james.parkhurst@diamond.ac.uk>
2021-01-18 11:14:01 +0000

    Parameterize test

--- multem-python/test/test_crystalline_materials.py
+++ multem-python/test/test_crystalline_materials.py
@@ -1,30 +1,32 @@
+import pytest
 import multem.crystalline_materials
 
+test_data = [
+    multem.crystalline_materials.Ag001_xtl,
+    multem.crystalline_materials.Ag110_xtl,
+    multem.crystalline_materials.Al001_xtl,
+    multem.crystalline_materials.Al110_xtl,
+    multem.crystalline_materials.Au001_xtl,
+    multem.crystalline_materials.Au110_xtl,
+    multem.crystalline_materials.Cu001_xtl,
+    multem.crystalline_materials.Cu110_xtl,
+    multem.crystalline_materials.Ir001_xtl,
+    multem.crystalline_materials.Ir110_xtl,
+    multem.crystalline_materials.Ni001_xtl,
+    multem.crystalline_materials.Ni110_xtl,
+    multem.crystalline_materials.Pb001_xtl,
+    multem.crystalline_materials.Pb110_xtl,
+    multem.crystalline_materials.Pd001_xtl,
+    multem.crystalline_materials.Pd110_xtl,
+    multem.crystalline_materials.Pt001_xtl,
+    multem.crystalline_materials.Pt110_xtl,
+]
 
-def check_crystal(Func):
+
+@pytest.mark.parametrize("Class", test_data)
+def test_crystal(Class):
     na = 10
     nb = 10
     nc = 10
     ncu = 1
-    atoms, lx, ly, lz, a, b, c, dz = Func(na, nb, nc, ncu)
-
-
-def test_crystalline_materials():
-    check_crystal(multem.crystalline_materials.Ag001_xtl)
-    check_crystal(multem.crystalline_materials.Ag110_xtl)
-    check_crystal(multem.crystalline_materials.Al001_xtl)
-    check_crystal(multem.crystalline_materials.Al110_xtl)
-    check_crystal(multem.crystalline_materials.Au001_xtl)
-    check_crystal(multem.crystalline_materials.Au110_xtl)
-    check_crystal(multem.crystalline_materials.Cu001_xtl)
-    check_crystal(multem.crystalline_materials.Cu110_xtl)
-    check_crystal(multem.crystalline_materials.Ir001_xtl)
-    check_crystal(multem.crystalline_materials.Ir110_xtl)
-    check_crystal(multem.crystalline_materials.Ni001_xtl)
-    check_crystal(multem.crystalline_materials.Ni110_xtl)
-    check_crystal(multem.crystalline_materials.Pb001_xtl)
-    check_crystal(multem.crystalline_materials.Pb110_xtl)
-    check_crystal(multem.crystalline_materials.Pd001_xtl)
-    check_crystal(multem.crystalline_materials.Pd110_xtl)
-    check_crystal(multem.crystalline_materials.Pt001_xtl)
-    check_crystal(multem.crystalline_materials.Pt110_xtl)
+    atoms, lx, ly, lz, a, b, c, dz = Class(na, nb, nc, ncu)
