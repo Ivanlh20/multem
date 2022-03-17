90fc1fd9e37c1f07989376530aa207b90c053099
Thomas Friedrich <thomas.friedrich87@outlook.de>
2022-03-17 16:10:47 +0100

    Add beam visualisation for CBED mode

--- matlab_functions/tfm_check_stem_setup.m
+++ matlab_functions/tfm_check_stem_setup.m
@@ -2,7 +2,9 @@ function tfm_check_stem_setup(input_multem, n_detector)
 
     fcn_check_probe(input_multem);
     fcn_check_slicing(input_multem);
-    fcn_check_detector_coverage(input_multem, n_detector);
+    if nargin > 1
+        fcn_check_detector_coverage(input_multem, n_detector);
+    end
 
     function fcn_check_probe(input_multem)
         input_multem.system_conf.precision = 1;                           % eP_Float = 1, eP_double = 2
@@ -54,27 +56,36 @@ function tfm_check_stem_setup(input_multem, n_detector)
         title('Slice positions');
         ylabel('z');
         xlabel('x');
-        axis equal;
         axis([min(atoms(:, 3)) max(atoms(:, 3)) -5 input_multem.spec_lz + 5]);
+        axis equal;
         legend({'Atom positions','Slice boundaries'},'Location','southoutside')
         set(gca,'YDir','reverse')
         
         subplot(1,2,2);
         plot3(input_multem.spec_atoms(:,2), input_multem.spec_atoms(:,3), input_multem.spec_atoms(:,4),'or');
         hold on;
-        plot3([input_multem.scanning_x0; input_multem.scanning_xe; input_multem.scanning_xe; input_multem.scanning_x0;input_multem.scanning_x0],...
-              [input_multem.scanning_y0; input_multem.scanning_y0; input_multem.scanning_ye; input_multem.scanning_ye;input_multem.scanning_y0],...
-              [0; 0; 0; 0; 0],'-k');
+        if input_multem.simulation_type <= 12 
+            plot3([input_multem.scanning_x0; input_multem.scanning_xe; input_multem.scanning_xe; input_multem.scanning_x0;input_multem.scanning_x0],...
+                  [input_multem.scanning_y0; input_multem.scanning_y0; input_multem.scanning_ye; input_multem.scanning_ye;input_multem.scanning_y0],...
+                  [0; 0; 0; 0; 0],'-k');
+            legend({'Atom positions','STEM Scan Field'},'Location','southoutside')
+        else
+            z0 = get_defocus_ref(input_multem);
+            [xp, yp, zp] = cylinder([0 2], 64);
+            surf(input_multem.iw_x+xp, input_multem.iw_y+yp, z0+(10*zp),'FaceAlpha',0.5,'EdgeColor','none','FaceColor','g')
+            surf(input_multem.iw_x+xp, input_multem.iw_y+yp, z0+(-10*zp),'FaceAlpha',0.5, 'EdgeColor','none','FaceColor','g')
+            plot3(input_multem.iw_x, input_multem.iw_y, z0,'xk');
+            legend({'Atom positions','Probe'},'Location','southoutside')
+        end
+
         hold off;
         axis equal;
         title('Atom positions and Scan Field')
         zlabel('z');
         xlabel('x');
         ylabel('y');
-        axis equal;
-        legend({'Atom positions','STEM Scan Field'},'Location','southoutside')
         set(gca,'ZDir','reverse')
-        view([0 0 1])
+
     end
 
     function fcn_check_detector_coverage(input_multem, detector)
@@ -123,5 +134,20 @@ function tfm_check_stem_setup(input_multem, n_detector)
         if tfm_pn_fact(ny,1)~=nx && tfm_pn_fact(ny,2)~=ny && tfm_pn_fact(ny,3)~=ny
            fprintf(2,['Using ' num2str(ny) ' pixels in y is computationally unefficient. Consider using ' num2str(tfm_pn_fact(ny,1)) ' or ' num2str(tfm_pn_fact(ny,3)) ' instead.'  '\n'])
         end
+    end
+end
+
+function z0 = get_defocus_ref(input_multem)
+    at_min = min(input_multem.spec_atoms(:,4));
+    at_max = max(input_multem.spec_atoms(:,4));
+    switch input_multem.cond_lens_zero_defocus_type 
+        case 1
+            z0 = at_min;
+        case 2
+            z0 = (at_max -at_min)/2+at_min;
+        case 3
+            z0 = at_max;
+        case 4
+            z0 = input_multem.cond_lens_zero_defocus_plane;
     end
 end
